973bfd89ae027140e0d41e6cb93efb1038a3c4ef
mrstephenneal <stephen.neal14@gmail.com>
2018-06-25 15:22:55 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='psdconvert',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=['psd_tools', 'Pillow', 'tqdm'],
+    url='https://github.com/mrstephenneal/psdconvert',
+    license='MIT License',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='PSD conversion package for python'
+)
